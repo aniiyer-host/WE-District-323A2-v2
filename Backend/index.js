4dbb17f7ec0876b29d99e9a3128eabcfeb513a5d
AkshMaheshwari <aksh.maheshwari05@gmail.com>
2025-12-04 15:26:08 +0530

    Backend initialised and database connected to mongo db

---
+++ Backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import connectDB from './models/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>{
+    res.send("API is running....");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ Backend/models/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // loads .env file
+
+const connectDB = async () => {
+  try {
+    // remove obsolete options
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected...");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
