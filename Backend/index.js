b52efcfc99835f59e3078a824d3b585f4d6105a9
aniiyer-host <aniketiyer4@gmail.com>
2026-01-30 18:31:59 +0530

    add test route 2.0

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.get("/api/health", (req, res) => {
+app.get("/", (req, res) => {
     res.send("API is running....");
 });
 
